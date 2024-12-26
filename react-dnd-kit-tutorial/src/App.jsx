19be5aee349da888418a511393768f5dc37b487e
kittyxcode <katherine.pino059@gmail.com>
2024-12-26 16:09:19 -0300

    feat: add useState hook and initialize people state in App component

--- react-dnd-kit-tutorial/src/App.jsx
+++ react-dnd-kit-tutorial/src/App.jsx
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+
+
+
 function App(){
+
+  const[people, setPeople] = useState([
+    {name: 'John', id: 1},
+    {name: 'Peter', id: 2},
+    {name: 'Sue', id: 3},])
+
   return(
     <div>hello world</div>
   )
