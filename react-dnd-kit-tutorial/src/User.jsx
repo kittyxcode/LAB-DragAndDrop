5d572103fc8a9773408b073c53bf93111bb44ca3
kittyxcode <katherine.pino059@gmail.com>
2024-12-27 18:45:39 -0300

    feat: add user component

--- react-dnd-kit-tutorial/src/User.jsx
+++ react-dnd-kit-tutorial/src/User.jsx
@@ -1,7 +1,24 @@
+import { useSortable } from "@dnd-kit/sortable";
+
+
 function User({user}){
+
+    const {
+        atributes, 
+        listeners,
+        setNodeRef, 
+        transform, 
+        transition
+    } = useSortable({id: user.id});
+
     return (
-        <div className='bg-white p-4 my-2 rounded shadow text-black'>
-            <h1>{user.name}</h1>
+        <div 
+            {...atributes}
+            {...listeners}
+            className='bg-white p-4 my-2 rounded shadow text-black'>
+            <h1>
+                {user.name}
+            </h1>
         </div>
     )
 }
