f5646599e95f401bfe74b2bec539fd705cdcb94b
kittyxcode <katherine.pino059@gmail.com>
2024-12-30 16:58:51 -0300

    fix: remove ghPages plugin from Vite configuration

--- react-dnd-kit-tutorial/vite.config.js
+++ react-dnd-kit-tutorial/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/LAB-DragAndDrop/',
-})
+  plugins: [react()],
+  base: '/LAB-DragAndDrop/', // Nombre exacto del repositorio
+});
+
