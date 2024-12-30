212539b72390a8aa186e0d7b2f3f20549ceba4ef
kittyxcode <katherine.pino059@gmail.com>
2024-12-30 15:46:06 -0300

    feat: enhance User component with sortable functionality and custom styles

--- react-dnd-kit-tutorial/src/User.jsx
+++ react-dnd-kit-tutorial/src/User.jsx
@@ -1,5 +1,5 @@
 import { useSortable } from "@dnd-kit/sortable";
-
+import {CSS} from '@dnd-kit/utilities';
 
 function User({user}){
 
@@ -12,8 +12,15 @@ function User({user}){
         transition
     } = useSortable({id: user.id});
 
+    const style={
+        transform: CSS.Transform.toString(transform),
+        transition,
+    }
+
     return (
         <div 
+            style={style}
+            ref={setNodeRef}
             {...atributes}
             {...listeners}
             className='bg-white p-4 my-2 rounded shadow text-black'>
