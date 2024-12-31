9e884b7cdc19156af2ac6605d7e7233c3944c826
kittyxcode <katherine.pino059@gmail.com>
2024-12-31 00:35:08 -0300

    fix: improve formatting and correct attribute naming in User component

--- react-dnd-kit-tutorial/src/User.jsx
+++ react-dnd-kit-tutorial/src/User.jsx
@@ -1,34 +1,33 @@
 import { useSortable } from "@dnd-kit/sortable";
-import {CSS} from '@dnd-kit/utilities';
+import { CSS } from '@dnd-kit/utilities';
 
-function User({user}){
+function User({ user }) {
+  
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition
+  } = useSortable({ id: user.id });
 
-    //continuuine
-    const {
-        atributes, 
-        listeners,
-        setNodeRef, 
-        transform, 
-        transition
-    } = useSortable({id: user.id});
 
-    const style={
-        transform: CSS.Transform.toString(transform),
-        transition,
-    }
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition: transition,
+  };
 
-    return (
-        <div 
-            style={style}
-            ref={setNodeRef}
-            {...atributes}
-            {...listeners}
-            className='bg-white p-4 my-2 rounded shadow text-black'>
-            <h1>
-                {user.name}
-            </h1>
-        </div>
-    )
+  return (
+    <div
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+      className='bg-white p-4 my-2 rounded shadow text-black'
+    >
+      <h1>{user.name}</h1>
+    </div>
+  );
 }
 
 export default User;
